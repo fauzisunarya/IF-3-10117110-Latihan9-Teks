5adab9c0d4f468ac8e88d63990ccaad41f0cae68
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-09-22 17:10:01 +0700

    first commit

---
+++ src/pkgif/pkg3/pkg10117110/latihan9/teks/IF310117110Latihan9Teks.java
@@ -0,0 +1,24 @@
+
+package pkgif.pkg3.pkg10117110.latihan9.teks;
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Sebuah Tipe data Char
+ */
+public class IF310117110Latihan9Teks {
+
+    public static void main(String[] args) {
+        char c;
+         char tab = '\t';
+         char newline = '\n';
+         c = 'A';
+         System.out.println("Hasil" + newline + "Char:" + tab + "berisi " + c);
+
+    }
+    
+}
